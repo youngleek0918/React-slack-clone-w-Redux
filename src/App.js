fc05353da00616c6a4120588a81be927d32c1ae0
youngleek0918 <youngleek0918@gmail.com>
2021-02-26 23:59:23 +0900

    built the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Slack clone w/ redux</h1>
+      <Router>
+        <>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
